203cef6a7cde6b9e6d2f7d284f5c4b893f7ed8e4
Alan Smith <a.bax.smith@gmail.com>
2014-08-05 15:32:05 -0600

    adds phone number logic- belongs to person-

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -17,4 +17,8 @@ let(:phone_number) { PhoneNumber.new(number: "1112223333", person_id: 1) }
     expect(phone_number).not_to be_valid
   end
 
+  it 'is associated with a person' do
+    expect(phone_number).to respond_to(:person)
+  end
+
 end
